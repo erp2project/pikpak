dd80c4d7986ad02acf216b31bf8fbb8dc23d5678
tkvkdldj <dlgmldn00@gmail.com>
2024-09-09 00:49:36 +0900

    0909 집에서 pikpak_lhw 업로드

--- src/main/java/kr/co/pikpak/controller/InoutBoundController.java
+++ src/main/java/kr/co/pikpak/controller/InoutBoundController.java
@@ -5,9 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class InoutBoundController {
-	
+	//입고요청 이동
 	@GetMapping("inoutbound/inboundreq")
 	public String inboundreq() {
+		return null;
+	}
+	
+	//가입고 등록 이동
+	@GetMapping("inoutbound/exreceiving")
+	public String exreceiving() {
+		return null;
+	}
+	
+	//입고 등록 이동
+	@GetMapping("inoutbound/recvenroll")
+	public String recvenroll() {
 		return null;
 	}
 	
--- src/main/webapp/resources/js/inoutbound/inboundreq_main.js
+++ src/main/webapp/resources/js/inoutbound/inboundreq_main.js
@@ -1,5 +1,13 @@
 export class page_move{
+	go_inboundreq(){
+		location.href = "./inboundreq"
+	}
+	
 	go_exreceiving(){
 		location.href = "./exreceiving"
+	}
+	
+	go_recvenroll(){
+		location.href = "./recvenroll"
 	}
 }
